c2f9c81a25d79a5f81f79ad13be3012ebf9256f4
Dominik Schneider <dominik.schneider@wsu.edu>
2019-06-25 18:09:45 -0700

    updated plots.R

--- scripts/plots.R
+++ scripts/plots.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(forcats)
 library(magrittr)
 
-data = read_csv('output/from_fluclight_data/output_psII_level0.csv',
+data = read_csv('output/from_example_data/output_psII_level0.csv',
                 na = 'nan')
 
 data %<>% mutate(exp = as_factor(exp),
@@ -14,18 +14,19 @@ data %<>% mutate(exp = as_factor(exp),
                                          TRUE ~ 'IndC'),
                  parameter = as_factor(parameter))
 
+
 summ = data %>% 
-  mutate(parameter = factor(parameter, levels = c('FvFm',seq(40,300,20)))) %>% 
   filter(frame %in% c('Fm','Fmp')) %>% 
   group_by(gtype, exp, date, parameter, measurement) %>% 
   summarise(avg = mean(yii_avg),
             std = mean(yii_avg))
 
 ggplot(summ %>% filter(parameter == 'FvFm')) +
-  geom_col(aes(y = avg, x = gtype, fill = exp), position='dodge')+
+  geom_col(aes(y = avg, x=gtype))+
   facet_wrap(~parameter)
 
 ggplot() + 
-  geom_point(data = summ %>% filter(parameter == 'FvFm'), aes(x = parameter, y = avg, color = gtype, shape = interaction(exp,gtype)))+
-  geom_line(data = summ %>% filter(parameter != 'FvFm'), aes(x = parameter, y = avg, group = interaction(exp,gtype), linetype = interaction(exp), color = gtype))
-
+  geom_line(data = summ %>% filter(parameter != 'FvFm'), aes(x = parameter, y = avg, group = interaction(exp,gtype), linetype = interaction(exp), color = gtype), size=1)+
+  geom_point(data = summ %>% filter(parameter == 'FvFm'), aes(x = parameter, y = avg, color = gtype, shape = interaction(exp)))+
+  scale_x_discrete(drop = FALSE)+
+  labs(title = 'yii')
