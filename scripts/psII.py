383508cef939665376cc9f907c07227f08328922
Dominik Schneider <dominik.schneider@wsu.edu>
2019-06-28 16:31:52 -0700

    small comment

--- scripts/psII.py
+++ scripts/psII.py
@@ -295,8 +295,7 @@ param_order = df.parameter.unique()
 df['parameter'] = pd.Categorical(df.parameter, categories=param_order, ordered=True)
 
 # # start testing
-# df2 = df.query('(treatment == "fluc" & sampleid == "tray1")')
-# nonfluc, 2019-06-21, 2019-06-21, tray1, 9, fluclight_data/pimframes/nonfluc-20190621-tray1-9.tif, Fp, 60, 4,
+# df2 = df.query('(treatment == "drought" & sampleid == "sample1")')
 # del df2
 # fundf = df2
 # fundf=df2.iloc[[2,3]]
